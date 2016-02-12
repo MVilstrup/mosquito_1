371da23072b5724f6c1f088be27643a0187a04e1
Mikkel Vilstrup <vilstrup@MacBook-Air-de-Mikkel.local>
2016-02-12 10:23:24 +0100

    Started on a setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Mosquito',
+      version='1.0',
+      description='Setup of Mosquito Webcrawler',
+      author='Mikkel Vilstrup',
+      author_email='Mikkel@Vilstrup.dk',
+      url='',
+      package_dir={'': 'utils'})
